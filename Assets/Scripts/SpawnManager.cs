c4a7963a7b8f46211f66dea0640a24ae29c4e095
Lucas Naspolini <67108278+Pixelikas@users.noreply.github.com>
2025-11-24 00:16:18 -0300

    Add files via upload

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -9,35 +9,32 @@ public class SpawnManager : MonoBehaviour
     private int posPlayer;
     private int ghostCount = 0;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
 
-        posPlayer = Random.Range(0,4);
-        Debug.Log(posPlayer);
+        SpawnPlayers();
+        
+    }
 
-        for(int i=0; i < 4; i++){
+    void SpawnPlayers(){
+
+        posPlayer = Random.Range(0,4);
+
+        for(int i=0; i < spawnPoints.Length; i++){
 
             if(i != posPlayer){
 
                 Instantiate(ghostsPrefabs[ghostCount], spawnPoints[i]);
                 ghostCount++;
-                Debug.Log("i ghost:" + i);
 
             }else{
 
                 Instantiate(playerPrefab, spawnPoints[i]);
-                Debug.Log("i player:" + i);
 
             }
 
         }
-        
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
